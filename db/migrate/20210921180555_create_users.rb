06b81bb01967b6c49e7a53fd405b50b7ecb17cbd
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-09-28 16:33:19 -0300

    Modify user original migration file to make email an index.

--- db/migrate/20210921180555_create_users.rb
+++ db/migrate/20210921180555_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false
+      t.index :email, unique: true
 
       t.timestamps
     end
