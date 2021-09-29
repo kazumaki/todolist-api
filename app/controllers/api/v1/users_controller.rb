4be2604639a31dc13a4ae5aff3867274ba6c9734
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-09-29 16:34:18 -0300

    add destroy method to users controller; add testing for users put actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,35 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user_by_id, only: [:show, :destroy]
 
   def index
     @users = User.all
-    render json: @users, status: 200
+    render json: @users, status: :ok
   end
 
   def create
-    @user = User.create!(user_params)
-    render json: @user, status: 201 
+    @user = User.new(user_params)
+    if @user.save then
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @user, status: :ok
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
   end
 
   private
+  def set_user_by_id
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:email)
+    params.permit(:email)
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Users", type: :request do
   let!(:users) { create_list(:user, 5) }
   let!(:user_id) { users.first.id }
 
-   # Test suite for GET /user
+  # Test suite for GET /user
 
   describe "GET /users" do
     # make HTTP get request before each example
@@ -20,6 +20,19 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe "GET /users/:id" do
+    before { get "/api/v1/users/#{user_id}"}
+
+    it 'return specific user' do
+      expect(json).not_to be_empty
+      expect(json['id']).to eq(user_id)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
   # Test suite for POST /user
 
   describe 'POST /user' do
@@ -27,7 +40,7 @@ RSpec.describe "Users", type: :request do
     let(:valid_email) { { email: 'mail@mail.com' } }
 
     context 'when the request is valid' do
-      before { post '/api/v1/users', params: { user: valid_email } }
+      before { post '/api/v1/users', params: valid_email }
       it 'creates a user' do
         expect(json['email']).to eq('mail@mail.com')
       end
@@ -49,8 +62,12 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe 'PUT /users/:id' do
+
+  end
+
   describe 'DELETE /users/:id' do
-    before { delete "api/v1/users/#{user_id}" }
+    before { delete "/api/v1/users/#{user_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
