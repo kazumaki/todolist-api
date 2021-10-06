f71ab743f0aa2c910b0c5fbe21f15b228479ce89
Vinicius Campos Carvalho <vcampos.pitangui@gmail.com>
2021-10-06 17:44:26 -0300

    add basic functionality to sessions controller to green the tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :set_user_by_email, only: %i[create]
+
+  def create
+    unless @user
+      
+    end
+
+    unless @user.authenticate(login_params[:password])
+
+    end
+
+    render json: {user: @user, token: JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base)}, status: :created
+  end
+
+  private
+  def set_user_by_email
+    @user = User.find_by(email: login_params[:email])
+  end
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe "Api::V1::Sessions", type: :request do
 
   describe "POST /sessions" do
     let(:valid_user) { User.create(email: 'mail@mail.com', password: 'meme123', password_confirmation: 'meme123')}
+
     context 'when the user data is valid' do
-      before { post 'api/v1/sessions', params: { user: { email: valid_user.email, password: 'meme123' } } }
+      before { post '/api/v1/sessions', params: { user: { email: valid_user.email, password: 'meme123' } } }
+
       it 'responds with a valid JWT' do
-        expect(JWT.decode(json['token'], Rails.application.secrets.secret_key_base)).to_not raise_error(JWT::DecodeError)
+        expect { JWT.decode(json["token"], Rails.application.secrets.secret_key_base) }.to_not raise_error(JWT::DecodeError)
       end
+
+      it { expect(response).to have_http_status(:created) }
     end
   end
 end
